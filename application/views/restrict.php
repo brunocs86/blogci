ad64c8cc4488b11bcd5e34ad6a161a8350c59761
Bruno Silva <brunocs86@outlook.com>
2019-07-17 11:25:37 -0300

    modificacao da pagina de acess0 restrito

--- application/views/restrict.php
+++ application/views/restrict.php
@@ -7,13 +7,29 @@
 
         <div class="row">
             <div class="col-lg-6 offset-lg-3 text-center">
-                <div class="row">
-                    <div class="col-lg-12 text-center">
-
-                    </div>
+                <div class="form-group">
+                    <a href="" class="btn btn-outline-dark"><i class="fa fa-user"></i></a>
+                    <a href="restrict/logoff" class="btn btn-outline-dark"><i class="fa fa-sign-out"></i></a>
                 </div>
             </div>
         </div>
-
+    </div>
+    <div class="container">
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link active" id="courses-tab" data-toggle="tab" href="#tab_courses" role="tab" aria-controls="courses" aria-selected="true">Cursos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="team-tab" data-toggle="tab" href="#tab_team" role="tab" aria-controls="team" aria-selected="false">Equipe</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="user-tab" data-toggle="tab" href="#tab_user" role="tab" aria-controls="user" aria-selected="false">Usuários</a>
+            </li>
+        </ul>
+        <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active" id="tab_courses" role="tabpanel" aria-labelledby="courses-tab">Conteúdo de Cursos</div>
+            <div class="tab-pane fade" id="tab_team" role="tabpanel" aria-labelledby="team-tab">Conteúdo de Equipe</div>
+            <div class="tab-pane fade" id="tab_user" role="tabpanel" aria-labelledby="user-tab">Conteúdo de Usuários</div>
+        </div>
     </div>
 </section><!-- #testimonials -->
